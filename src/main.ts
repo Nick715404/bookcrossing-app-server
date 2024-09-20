a7c120e13434945d7e1d735e8e8b09c0f2a6b618
Nick715404 <nikita.vasia2015@gmail.com>
2024-09-20 14:10:53 +0500

    upt: Create schema folder and change .env and .env.local in gitignore

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as fs from 'fs';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
