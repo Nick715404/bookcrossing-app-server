c53a5633450f3004880dd9935f98fcbeb0940fc4
Nick715404 <nikita.vasia2015@gmail.com>
2024-09-20 10:18:04 +0500

    fix: still checking problems

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { join } from 'path';
 import * as fs from 'fs';
 
 async function bootstrap() {
+	console.log(process.env.SSL_KEY_PATH);
+	console.log(process.env.SSL_CERT_PATH);
+
 	const httpsOptions = {
 		key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`),
 		cert: fs.readFileSync(`${process.env.SSL_CERT_PATH}`),
 	};
-
-	console.log(process.env.SSL_KEY_PATH);
-	console.log(process.env.SSL_CERT_PATH);
 
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		httpsOptions,
