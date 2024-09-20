374075c78dcf970d89ebefff9c0f8fc132a73f17
Nick715404 <nikita.vasia2015@gmail.com>
2024-09-20 09:26:51 +0500

    upt: change env.local

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,94 +1,89 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { UpdateImageDto } from './dto/update-image.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import * as fs from 'fs/promises'
-const { join } = require("path");
-
+import * as fs from 'fs/promises';
+const { join } = require('path');
 
 @Injectable()
 export class ImageService {
-  constructor(private readonly prismaService: PrismaService) { }
+	constructor(private readonly prismaService: PrismaService) {}
 
-  async load(images: any, bookId: string) {
-    try {
-      const savedImages = [];
+	async load(images: any, bookId: string) {
+		try {
+			const savedImages = [];
 
-      for (const image of images) {
-        const timeStamp = new Date().toISOString().replace(/[-:.]/g, '');
-        const imagePath = `images/${timeStamp}_${image.originalname}`;
+			for (const image of images) {
+				const timeStamp = new Date().toISOString().replace(/[-:.]/g, '');
+				const imagePath = `images/${timeStamp}_${image.originalname}`;
 
-        const rootPath = join(__dirname, "../../uploads");
-        // console.log(rootPath);
+				const rootPath = join(__dirname, '../../uploads');
 
-        savedImages.push(imagePath);
-        await fs.writeFile(`${rootPath}/${imagePath}`, image.buffer);
-      }
+				savedImages.push(imagePath);
+				await fs.writeFile(`${rootPath}/${imagePath}`, image.buffer);
+			}
 
-      const book = await this.prismaService.book.findUnique({
-        where: {
-          id: bookId
-        }
-      })
+			const book = await this.prismaService.book.findUnique({
+				where: {
+					id: bookId,
+				},
+			});
 
-      if (!book) {
-        throw new Error('Книга не найдена');
-      }
+			if (!book) {
+				throw new Error('Книга не найдена');
+			}
 
-      const createdImages = await this.prismaService.image.createMany({
-        data: savedImages.map((path) => ({
-          path,
-          bookId,
-          filename: path.split('/').pop(),
-        }))
-      });
+			const createdImages = await this.prismaService.image.createMany({
+				data: savedImages.map(path => ({
+					path,
+					bookId,
+					filename: path.split('/').pop(),
+				})),
+			});
 
-      return createdImages;
-    }
-    catch (error) {
-      throw new HttpException(error.message, HttpStatus.CONFLICT);
-    }
-  }
+			return createdImages;
+		} catch (error) {
+			throw new HttpException(error.message, HttpStatus.CONFLICT);
+		}
+	}
 
-  findAll() {
-    return `This action returns all image`;
-  }
+	findAll() {
+		return `This action returns all image`;
+	}
 
-  async updateImages(images: any, bookId: string) {
-    try {
-      const savedImages = [];
+	async updateImages(images: any, bookId: string) {
+		try {
+			const savedImages = [];
 
-      for (const image of images) {
-        const timeStamp = new Date().toISOString().replace(/[-:.]/g, '');
-        const imagePath = `images/${timeStamp}_${image.originalname}`;
-        const rootPath = join(__dirname, "../../uploads");
+			for (const image of images) {
+				const timeStamp = new Date().toISOString().replace(/[-:.]/g, '');
+				const imagePath = `images/${timeStamp}_${image.originalname}`;
+				const rootPath = join(__dirname, '../../uploads');
 
-        savedImages.push(imagePath);
-        await fs.writeFile(`${rootPath}/${imagePath}`, image.buffer);
-      }
+				savedImages.push(imagePath);
+				await fs.writeFile(`${rootPath}/${imagePath}`, image.buffer);
+			}
 
-      const createdImages = await this.prismaService.image.createMany({
-        data: savedImages.map((path) => ({
-          path,
-          bookId,
-          filename: path.split('/').pop(),
-        }))
-      });
-    }
-    catch (error) {
-      throw new Error(`Failed to update images: ${error.message}`);
-    }
-  }
+			const createdImages = await this.prismaService.image.createMany({
+				data: savedImages.map(path => ({
+					path,
+					bookId,
+					filename: path.split('/').pop(),
+				})),
+			});
+		} catch (error) {
+			throw new Error(`Failed to update images: ${error.message}`);
+		}
+	}
 
-  async findOne(id: string) {
-    const bookImages = await this.prismaService.image.findFirst({
-      where: {
-        bookId: id
-      }
-    })
-    return bookImages;
-  }
+	async findOne(id: string) {
+		const bookImages = await this.prismaService.image.findFirst({
+			where: {
+				bookId: id,
+			},
+		});
+		return bookImages;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} image`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} image`;
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,118 +4,113 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+	constructor(private readonly prismaService: PrismaService) {}
 
-  constructor(private readonly prismaService: PrismaService) { }
+	async create(data: CreateUserDto) {
+		try {
+			const user = await this.CreateUser(data);
+			const shelf = await this.CreateShelf(user.userId);
+			const favorites = await this.CreateFavorites(user.userId);
 
-  async create(data: CreateUserDto) {
-    try {
-      const user = await this.CreateUser(data)
-      const shelf = await this.CreateShelf(user.userId)
-      const favorites = await this.CreateFavorites(user.userId);
+			return { user: user, shelf: shelf, favorites: favorites };
+		} catch (error) {
+			throw new HttpException(error.message, HttpStatus.CONFLICT);
+		}
+	}
 
-      return { user: user, shelf: shelf, favorites: favorites }
+	async CreateUser(data: CreateUserDto) {
+		return await this.prismaService.user.create({
+			data: {
+				vkId: data.id,
+				city: data.city.title,
+				name: data.first_name,
+				surName: data.last_name,
+				avatar: data.photo_100,
+			},
+		});
+	}
 
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.CONFLICT)
-    }
-  }
+	async CreateShelf(userId: string) {
+		return this.prismaService.shelf.create({
+			data: {
+				userId: userId,
+				books: {
+					create: [],
+				},
+			},
+		});
+	}
 
-  async CreateUser(data: CreateUserDto) {
-    return await this.prismaService.user.create({
-      data: {
-        vkId: data.id,
-        city: data.city.title,
-        name: data.first_name,
-        surName: data.last_name,
-        avatar: data.photo_100
-      }
-    });
-  }
+	async CreateFavorites(userId: string) {
+		return await this.prismaService.favourites.create({
+			data: {
+				user: userId,
+				books: {
+					create: [],
+				},
+			},
+		});
+	}
 
-  async CreateShelf(userId: string) {
-    return this.prismaService.shelf.create({
-      data: {
-        userId: userId,
-        books: {
-          create: []
-        }
-      }
-    });
-  }
+	async findAll() {
+		try {
+			const users = await this.prismaService.user.findMany();
+			return users;
+		} catch (error) {
+			throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  async CreateFavorites(userId: string) {
-    return await this.prismaService.favourites.create({
-      data: {
-        user: userId,
-        books: {
-          create: []
-        }
-      }
-    })
-  }
+	async findOne(id: number) {
+		try {
+			const user = await this.FindCurrentUser(id);
+			if (!user) {
+				throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+			}
+			return user;
+		} catch (error) {
+			throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  async findAll() {
-    try {
-      const users = await this.prismaService.user.findMany();
-      return users;
-    }
-    catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
+	async FindCurrentUser(id: number) {
+		const userVkId = typeof id === 'string' ? parseInt(id, 10) : id;
+		return this.prismaService.user.findFirst({
+			where: {
+				vkId: userVkId,
+			},
+		});
+	}
 
-  async findOne(id: number) {
-    try {
-      const user = await this.FindCurrentUser(id);
-      if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-      }
-      return user;
-    }
-    catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  };
+	async FindShelfByUserId(userId: string) {
+		return this.prismaService.shelf.findUnique({
+			where: {
+				userId: userId,
+			},
+		});
+	}
 
-  async FindCurrentUser(id: number) {
-    const userVkId = typeof id === 'string' ? parseInt(id, 10) : id;
-    return this.prismaService.user.findFirst({
-      where: {
-        vkId: userVkId
-      }
-    });
-  };
+	async remove(id: string) {
+		try {
+			const deletedShelf = await this.DeleteShelf(id);
 
-  async FindShelfByUserId(userId: string) {
-    return this.prismaService.shelf.findUnique({
-      where: {
-        userId: userId
-      }
-    });
-  };
+			const deletedUser = await this.prismaService.user.delete({
+				where: {
+					userId: id,
+				},
+			});
 
-  async remove(id: string) {
-    try {
-      const deletedShelf = await this.DeleteShelf(id);
+			return { user: deletedUser, shelf: deletedShelf };
+		} catch (error) {
+			throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-      const deletedUser = await this.prismaService.user.delete({
-        where: {
-          userId: id
-        }
-      });
-
-      return { user: deletedUser, shelf: deletedShelf };
-    }
-    catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  async DeleteShelf(id: string) {
-    return this.prismaService.shelf.delete({
-      where: {
-        userId: id
-      }
-    });
-  }
+	async DeleteShelf(id: string) {
+		return this.prismaService.shelf.delete({
+			where: {
+				userId: id,
+			},
+		});
+	}
 }
