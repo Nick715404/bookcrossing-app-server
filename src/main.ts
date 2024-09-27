a3bfb324426f246e5de753d1e706f1a3f5ec0b64
Nick715404 <nikita.vasia2015@gmail.com>
2024-09-27 10:06:04 +0500

    fix: Поправил файл main.ts. Сделал в нем опции для ссл

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const httpsOptions = {
+		key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`),
+		cert: fs.readFileSync(`${process.env.SSL_CERT_PATH}`),
+	};
+
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		httpsOptions,
+	});
 
 	app.setGlobalPrefix('api');
 	app.enableCors({
