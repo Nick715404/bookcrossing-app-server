41eb46f0a26557b6df2435257a87457afad24757
Nick715404 <nikita.vasia2015@gmail.com>
2024-09-27 10:12:59 +0500

    fix: Сделал так, чтобы файл main.ts на ветке main был не изменным

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as fs from 'fs';
 
 async function bootstrap() {
-	const httpsOptions = {
-		key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`),
-		cert: fs.readFileSync(`${process.env.SSL_CERT_PATH}`),
-	};
-
-	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-		httpsOptions,
-	});
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
 	app.setGlobalPrefix('api');
 	app.enableCors({
